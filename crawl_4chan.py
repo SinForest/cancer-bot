8568f26e0f97b12b0e49bb44ce4d7120f2cf1f93
Patrick Dammann <github@thesinforest.eu>
2017-08-30 22:31:06 +0200

    Added endless loop

--- crawl_4chan.py
+++ crawl_4chan.py
@@ -38,45 +38,61 @@ def clear_post(comment):
 
 if __name__ == '__main__':
 
-    # login database
-    db = records.Database("postgresql://{}/cancer-corpus".format(open(AUTH_SECRET, "r").read().split()[0]))
+    it_count = 0
+    er_count = 0
+    start_time = time()
+    while True:
 
-    # iterate over all interesting boards
-    for board_name in BOARD_LIST:
-        r = wait_get("https://a.4cdn.org/{}/threads.json".format(board_name))
-        board = r.json()
-        relsize = db.query_file(DB_SIZE_SQL)[0]["total"]
-        postcnt = db.query("select count(*) as count from fourchan")[0]["count"]
-        print("\033[36m[CRAWLING /{}/]\033[37m database_size:{}; post_count:{}".format(board_name, relsize, postcnt))
+        try:
+            it_count += 1
+            running_time = (time() - start_time) / 3600
+            print("\033[31m[ITERATION {}]\033[37m Running for {:.2f}h with {} errors. [{:.2f} IT/s]".format(it_count, running_time, er_count, running_time / it_count))
 
-        # iterate over all pages (11) in the board
-        for page in board:
-            print("Crawling 4chan.org/{}/{}".format(board_name, page["page"]))
-            post_counter = 0
+            # login database
+            db = records.Database("postgresql://{}/cancer-corpus".format(open(AUTH_SECRET, "r").read().split()[0]))
 
-            # iterate over all threads (15) on the page
-            for thread in tqdm(page["threads"]):
-                r = wait_get("https://a.4cdn.org/{}/thread/{}.json".format(board_name, thread["no"]))
-                try: # this sometimes raises strange JSON errors
-                    thread = r.json()
-                except:
-                    continue
+            # iterate over all interesting boards
+            for board_name in BOARD_LIST:
+                r = wait_get("https://a.4cdn.org/{}/threads.json".format(board_name))
+                board = r.json()
+                relsize = db.query_file(DB_SIZE_SQL)[0]["total"]
+                postcnt = db.query("select count(*) as count from fourchan")[0]["count"]
+                print("\033[36m[CRAWLING /{}/]\033[37m database_size:{}; post_count:{}".format(board_name, relsize, postcnt))
 
-                # iterate over all posts (>= 1) in the thread
-                for post in thread["posts"]:
-                    if "com" in post and "no" in post:
+                # iterate over all pages (11) in the board
+                for page in board:
+                    print("Crawling 4chan.org/{}/{}".format(board_name, page["page"]))
+                    post_counter = 0
 
-                        # test if post already existing
-                        r = db.query("SELECT count(1) AS out FROM fourchan WHERE post_id = :id", id=post['no'])
-                        if r[0]["out"] != 0: continue
+                    # iterate over all threads (15) on the page
+                    for thread in tqdm(page["threads"]):
+                        r = wait_get("https://a.4cdn.org/{}/thread/{}.json".format(board_name, thread["no"]))
+                        try: # this sometimes raises strange JSON errors
+                            thread = r.json()
+                        except:
+                            continue
 
-                        # process comment
-                        comment = clear_post(post["com"])
-                        if comment == "": continue
-                        post_counter += 1
+                        # iterate over all posts (>= 1) in the thread
+                        for post in thread["posts"]:
+                            if "com" in post and "no" in post:
 
-                        # insert comment into database
-                        db.query("INSERT INTO fourchan\
-                                  VALUES (:id, :com)",
-                                  id=post["no"], com=comment)
-            print("  - Added {} new posts!".format(post_counter))
+                                # test if post already existing
+                                r = db.query("SELECT count(1) AS out FROM fourchan WHERE post_id = :id", id=post['no'])
+                                if r[0]["out"] != 0: continue
+
+                                # process comment
+                                comment = clear_post(post["com"])
+                                if comment == "": continue
+                                post_counter += 1
+
+                                # insert comment into database
+                                db.query("INSERT INTO fourchan\
+                                          VALUES (:id, :com)",
+                                          id=post["no"], com=comment)
+                    print("  - Added {} new posts!".format(post_counter))
+
+        except KeyboardInterrupt as e:
+            raise e
+        except:
+            er_count += 1
+            continue
